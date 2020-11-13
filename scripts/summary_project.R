0745830dced2fbe14c7e33f3e9a52f79c4c311a5
ereyand <ericar623@gmail.com>
2020-11-12 22:44:48 -0800

    Add dataset

--- scripts/summary_project.R
+++ scripts/summary_project.R
@@ -1,2 +1,7 @@
 # summary information w/values
 # Dataset with Alabama homelessness population
+
+# load the dataset
+alabama <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-ereyand/gh-pages/panel_2_table_and_counts_v7_2020_03_27.csv")
+
+# view it
