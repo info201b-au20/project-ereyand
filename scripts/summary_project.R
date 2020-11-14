9b3a2fa9699d7b5a5dce0c8ae5f92038de894b7b
ereyand <ericar623@gmail.com>
2020-11-13 16:56:09 -0800

    Clarify features and obs

--- scripts/summary_project.R
+++ scripts/summary_project.R
@@ -1,7 +1,15 @@
 # summary information w/values
 # Dataset with Alabama homelessness population
+library(tidyverse)
+library(stringr)
 
 # load the dataset
 alabama <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-ereyand/gh-pages/panel_2_table_and_counts_v7_2020_03_27.csv")
 
 # view it
+# Number of observations in dataset
+obs_alabama <- nrow(alabama)
+
+# Number of features in dataset
+num_features_alabama <- ncol(alabama)
+
