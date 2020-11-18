edc4e646df48ae7d5b43e27d2384f41058ec8dd6
jsand24-1962972 <56006088+jsand24-1962972@users.noreply.github.com>
2020-11-17 21:25:53 -0800

    Update summary_chart.R

--- scripts/summary_chart.R
+++ scripts/summary_chart.R
@@ -1,1 +1,14 @@
 # summary chart
+# load tidyverse and stringr
+library(tidyverse)
+library(stringr)
+national <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-ereyand/gh-pages/panel_2_table_and_counts_v7_2020_03_27.csv")
+# Group CoC
+homelessness_summary <- national %>% 
+  mutate(state = str_sub(coc_number, 1, 2 )) %>%
+  filter(!is.na(chronically_homeless)) %>%
+  group_by(state) %>% 
+  summarise(chronically_homeless = max(chronically_homeless), 
+            homeless_veterans = max(homeless_veterans),
+            sheltered_youth = max(sheltered_total_homeless_unaccompanied_youth_under_25))
+view(homelessness_summary)
