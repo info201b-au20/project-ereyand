d19c1e9d2d83111129a3ca556bf84d657eb911c7
ANUSHMR <ANUSHMR@UW.EDU>
2020-11-25 21:45:30 -0800

    charts

--- scripts/first_chart.R
+++ scripts/first_chart.R
@@ -1,40 +1,40 @@
-# first chart
-# West Coast Homeless veterans
-# Load packages
-library(ggplot2)
+# Chart 1 by Anush - Columns
 library(tidyverse)
+library(ggplot2)
 library(stringr)
 
-# Load raw data
-raw_data <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-ereyand/gh-pages/panel_2_table_and_counts_v7_2020_03_27.csv")
+# Load the working data
+data_raw <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-ereyand/gh-pages/panel_2_table_and_counts_v7_2020_03_27.csv")
 
-# Load data
-data <- raw_data %>% 
+making_homeless <- data_raw %>% 
+  select(homeless_individuals, homeless_veterans, coc_number) %>% 
   mutate(state = str_sub(coc_number, start = 1, end = 2))
 
 
-homeless_data <- data %>% 
-  group_by(state) %>% 
-  mutate(sheltered_total_homeless_veterans = 
-           as.numeric(sheltered_total_homeless_veterans, na.rm = TRUE)) %>% 
-  mutate(unsheltered_homeless_veterans = 
-           as.numeric(unsheltered_homeless_veterans, na.rm = TRUE)) %>% 
-  select(sheltered_total_homeless_veterans, unsheltered_homeless_veterans)
+# States with highest numbers of homeless individuals and veterans
+homeless_ind <- making_homeless %>%
+  group_by(state) %>%
+  filter(homeless_individuals == max(homeless_individuals)) %>%
+  select(homeless_individuals)
 
-# Washington state veteran data
-homeless_wa <- homeless_data %>% 
-  filter(state == "WA")
+homeless_vet <- making_homeless %>%
+  group_by(state) %>%
+  filter(homeless_veterans == max(homeless_veterans)) %>%
+  select(homeless_veterans)
 
-wa_plot <- ggplot(data = homeless_wa) +
-  geom_point(mapping = aes(x = unsheltered_homeless_veterans,
-                           y = sheltered_total_homeless_veterans)) +
-  ggtitle("Homeless Veterans in Washington")
+# Highest Number of Homeless Individuals by State
+individuals <- ggplot(data = homeless_ind) +
+  geom_col(mapping = aes(x = state, y = homeless_individuals, fill = state)) +
+  coord_flip() +
+labs(x = "State", y = "Homeless Individuals",
+     title = "Highest Number of Homeless Individuals by State")
 
-# California state veteran data
-homeless_ca <- homeless_data %>% 
-  filter(state == "CA")
+# Highest Number of Homeless Veterans by State
+vets <- ggplot(data = homeless_vet) +
+  geom_col(mapping = aes(x = state, y = homeless_veterans, fill = state)) +
+  coord_flip() +
+  labs(x = "State", y = "Homeless Veterans",
+       title = "Highest Number of Homeless Veterans by State")
 
-ca_plot <- ggplot(data = homeless_ca) +
-  geom_point(mapping = aes(x = unsheltered_homeless_veterans,
-                           y = sheltered_total_homeless_veterans)) +
-  ggtitle("Homeless Veterans in California")
+comparison_plots <- individuals/vets
+
--- scripts/third_chart.R
+++ scripts/third_chart.R
@@ -1,41 +1,39 @@
-# third chart
-# Load packages
-library(ggplot2)
+# Chart 3 by Anush - Points
 library(tidyverse)
+library(ggplot2)
 library(stringr)
 
-# Load raw data
-raw_data <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-ereyand/gh-pages/panel_2_table_and_counts_v7_2020_03_27.csv")
+# Load the working data
+data_raw <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-ereyand/gh-pages/panel_2_table_and_counts_v7_2020_03_27.csv")
 
-# Load data
-data <- raw_data %>% 
+making_homeless <- data_raw %>% 
+  select(homeless_individuals, homeless_veterans, coc_number) %>% 
   mutate(state = str_sub(coc_number, start = 1, end = 2))
 
 
-homeless_data <- data %>% 
-  group_by(state) %>% 
-  mutate(sheltered_total_homeless_veterans = 
-           as.numeric(sheltered_total_homeless_veterans, na.rm = TRUE)) %>% 
-  mutate(unsheltered_homeless_veterans = 
-           as.numeric(unsheltered_homeless_veterans, na.rm = TRUE)) %>% 
-  select(sheltered_total_homeless_veterans, unsheltered_homeless_veterans)
+# States with highest numbers of homeless individuals and veterans
+homeless_ind <- making_homeless %>%
+  group_by(state) %>%
+  filter(homeless_individuals == max(homeless_individuals)) %>%
+  select(homeless_individuals)
 
-# Florida state homeless data
-homeless_fl <- homeless_data %>% 
-  filter(state == "FL")
+homeless_vet <- making_homeless %>%
+  group_by(state) %>%
+  filter(homeless_veterans == max(homeless_veterans)) %>%
+  select(homeless_veterans)
 
-fl_plot <- ggplot(data = homeless_fl) +
-  geom_point(mapping = aes(x = unsheltered_homeless_veterans,
-                           y = sheltered_total_homeless_veterans)) +
-  ggtitle("Homeless Veterans in Florida")
+# Highest Number of Homeless Vets by State
+ind_point <- ggplot(data = homeless_ind) +
+  geom_point(mapping = aes(x = state, y = homeless_individuals, color = state)) +
+  labs(x = "State", y = "Homeless Individuals",
+       title = "Highest Number of Homeless Individuals by State")
 
-# New York state homeless data
-homeless_ny <- homeless_data %>% 
-  filter(state == "NY")
+# Highest Number of Homeless Veterans by State
+vets_point <- ggplot(data = homeless_vet) +
+  geom_point(mapping = aes(x = state, y = homeless_veterans, color = state)) +
+  labs(x = "State", y = "Homeless Veterans",
+       title = "Highest Number of Homeless Veterans by State")
 
-ny_plot <- ggplot(data = homeless_ny) +
-  geom_point(mapping = aes(x = unsheltered_homeless_veterans,
-                           y = sheltered_total_homeless_veterans)) +
-  ggtitle("Homeless Veterans in New York")
+comparison_plots <- ind_point/vets_point
 
 
