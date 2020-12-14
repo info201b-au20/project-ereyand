73d0253c545eab0db3d259fd1185e2da18f02c3c
Jalen Sanders <jsand24@uw.edu>
2020-12-13 23:30:08 -0800

    Commits

--- app_server.R
+++ app_server.R
@@ -7,6 +7,20 @@ library(ggplot2)
 library(plotly)
 
 national <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-ereyand/gh-pages/panel_2_table_and_counts_v7_2020_03_27.csv", stringsAsFactors = F)
+summary_df <- read_excel("homelessness_sum_new.xlsx", 
+            col_types = c("text", "text", "numeric"))
+# Summary Plot for chronically homeless, homeless veterans and homeless youth
+make_sum_plot <- function(df, states) {
+  
+  df <- df %>%
+    filter(state == states) 
+  
+sum_plot <- ggplot(data = df, mapping = aes(x = homeless_group, y = coc_population)) +
+    geom_bar(aes(color = "red", stat = "identity") 
+ )
+}
+
+
 
 # Create server variable
 
@@ -25,4 +39,13 @@ server <- function(input, output) {
     }
     p
   })
+  summary_df$homeless_group <- factor(
+    summary_df$homeless_group, 
+    levels = c("chronically_homeless", "homeless_veterans", "sheltered_youth"))
+
+  output$sum_plot <- renderPlotly({
+    return(make_sum_plot(summary_df, input$states))
+    
+  })
+  
 }
