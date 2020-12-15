68fb6142b278e24c14b4ef9be61c784d057a80cb
ereyand <ericar623@gmail.com>
2020-12-15 10:58:23 -0800

    Add chart

--- app_server.R
+++ app_server.R
@@ -8,6 +8,11 @@ library(plotly)
 
 national <- read.csv("https://raw.githubusercontent.com/info201b-au20/project-ereyand/gh-pages/panel_2_table_and_counts_v7_2020_03_27.csv", stringsAsFactors = F)
 
+# chart two data
+# isolate certain columns that show different groups of people experiencing
+# homelessness
+# create feature input that has the available columns as choices
+
 # Create server variable
 
 server <- function(input, output) {
@@ -16,7 +21,8 @@ server <- function(input, output) {
       filter(total_homeless > 0)
     p <- ggplot(
       data = plot_data,
-      mapping = aes_string(x = input$feature, y = national$total_homeless, color = "coc_number")
+      mapping = aes_string(x = input$feature, y = national$total_homeless, 
+                           color = "coc_number")
     ) + labs(x = "Area", y = "Total Number of Homeless Individuals") +
       geom_point()
     
@@ -24,5 +30,11 @@ server <- function(input, output) {
       p <- p + geom_smooth(se = FALSE)
     }
     p
+  })
+  
+  output$scatter_plot <- renderPlot({
+    plot <- ggplot(data = national) +
+      geom_point(mapping = aes_string(x = input$feature, y = total_homeless, 
+                                      color = ))
   })
 }
